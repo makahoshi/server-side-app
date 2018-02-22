ec9cef16d4ad524345dc4125743fd31d63e4ffa7
Makaila Akahoshi <mkakahoshi@gmail.com>
2018-02-22 15:44:57 -0500

    adding webpackNodeExternals in the server webpack

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
   //inform webpack that we're building a bundle for nodejs, rather than the browser
@@ -14,7 +15,11 @@ const config = {
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, 'build')
-    }
+    },
+    //tells webpack not to bundle any libraries into our output bundle on the server
+    //if that library exists inside the node modules folder
+    //anything that is in our node_modules folder will not be bundled in our server bundle
+    externals : [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
