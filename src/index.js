4947c9d5d11bd63557319111d24c97d09bf01cd8
Makaila Akahoshi <mkakahoshi@gmail.com>
2018-02-22 15:50:10 -0500

    separating the react logic and the express logic

--- src/index.js
+++ src/index.js
@@ -1,25 +1,12 @@
 import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import Home from './client/components/Home';
+import renderer from './helpers/renderer';
 
 const app = express();
 
 app.use(express.static('public'));
 app.get('/', (req, res) => {
     //getting the bundle,js file from public folder
-    const content = renderToString(<Home/>); //this is the html of our app
-
-    const html = `
-    <html>
-    <head></head>
-    <body>
-    <div id="root">${content}</div>
-    <script src="bundle.js"></script>
-    </body>
-    </html>`;
-
-    res.send(html);
+    res.send(renderer());
 });
 
 
