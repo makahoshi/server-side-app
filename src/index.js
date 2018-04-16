48a74c204c4088f9a8bf7052260e2c81be08bf02
Makaila Akahoshi <mkakahoshi@gmail.com>
2018-04-16 11:51:05 -0400

    configure matchRoutes function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import 'babel-polyfill';
 import express from 'express';
+import Routes from './client/Routes';
+import { matchRoutes } from 'react-router-config';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
 
@@ -10,6 +12,9 @@ app.get('*', (req, res) => {
     //getting the bundle,js file from public folder
     const store = createStore();
     //some logic to initialize and load date into the store
+    //calling matchRoutes to determine based on the route what component needs to be rendered
+    //matchRoutes looks at the array of routes and the route path and then will return an array of components that will be rendered
+    matchRoutes(Routes, req.path)
     res.send(renderer(req, store));
 });
 
